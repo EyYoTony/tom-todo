1ecd6943528f25eb1191bc54afdce5918e25a613
George Worthington <GeoWorthington@gmail.com>
2017-06-08 17:07:46 -0400

    commented out mapaccum function

--- cmds/rm.js
+++ cmds/rm.js
@@ -1,13 +1,21 @@
 const { get, set } = require('../lib/store.js')
 const ls = require('./ls')
-const { filter, curry } = require('ramda')
+const { filter, curry, mapAccum } = require('ramda')
 
 module.exports = function(id){
 const list = get()
 set(filter(x => !isId(id,x),list))
+//set(mapAccum(reducer, 1, list)[0])
 ls()
 }
+
 
 function  isId(idIn, obj) {
 return obj.id  == idIn
 }
+
+// function reducer(acc, value){
+//   value.id = acc
+//   acc += 1
+//   return [acc,value]
+// }
